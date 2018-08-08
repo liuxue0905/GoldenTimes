1445977bffcd897ca6c31ca7512ab424499744c3
liuxue0905 <liuxue0905@hotmail.com>
2018-08-09 00:24:42 +0800

    mdc修改唱片列表。

--- mdc/app.js
+++ mdc/app.js
@@ -1,14 +1,18 @@
-import * as mdc from 'material-components-web';
+// import * as mdc from 'material-components-web';
+
+// console.log('abc');
+// console.log('mdc', mdc);
+// window.mdc = mdc;
 
 import {MDCRipple} from '@material/ripple';
 import {MDCSelect} from '@material/select';
 import {MDCTextField} from '@material/textfield';
 
-const shippingForm = document.querySelector('#crane-shipping-form');
-shippingForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  alert('Success!');
-});
+// const shippingForm = document.querySelector('#crane-shipping-form');
+// shippingForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   alert('Success!');
+// });
 
 new MDCRipple(document.querySelector('.mdc-button'));
 new MDCSelect(document.querySelector('.mdc-select'));
@@ -18,8 +22,5 @@ textFieldElements.forEach((textFieldEl) => {
   new MDCTextField(textFieldEl);
 });
 
-const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
-iconButtonRipple.unbounded = true;
-
-// const surface = document.querySelector('.abc');
-// const ripple = new MDCRipple(surface);
+// const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+// iconButtonRipple.unbounded = true;
