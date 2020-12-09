51aaa9327425439700612f5ff961670205709f16
liuxue <liuxue0905@hotmail.com>
2020-12-09 16:18:40 +0800

    修改settings

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = multiprocessing.cpu_count() * 2
+
+print("bind", bind)
+print("workers", workers)
+print("threads", threads)
--- runserver.sh
+++ runserver.sh
@@ -34,7 +34,8 @@ LOG_FILE=${LOG_DIR}/nohup_${LOG_FILE_NAME}.out
 echo "LOG_FILE	: ${LOG_FILE}"
 
 #COMMAND_RUNSERVER="python manage.py runserver 0:8000"
-COMMAND_RUNSERVER="gunicorn -b 0:8000 -w 3 --threads 2 GoldenTimes.wsgi"
+#COMMAND_RUNSERVER="gunicorn -b 0:8000 -w 3 --threads 2 GoldenTimes.wsgi"
+COMMAND_RUNSERVER="gunicorn GoldenTimes.wsgi -c gunicorn.conf.py"
 COMMAND="nohup ${COMMAND_RUNSERVER} > ${LOG_FILE} 2>&1 &"
 
 echo "COMMAND		: ${COMMAND}"
