0c796b7603c06c40f8a1a3436b77a0e98acc55de
liuxue <liuxue0905@hotmail.com>
2020-12-10 14:20:28 +0800

    修改settings

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,10 @@
 import multiprocessing
 
 bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = multiprocessing.cpu_count() * 2
+# workers = multiprocessing.cpu_count() * 2 + 1
+# threads = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count()
+threads = 2
 
 print("bind", bind)
 print("workers", workers)
