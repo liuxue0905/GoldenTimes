690d12f7c9a3edb8ff1103c790e967b40c02a1e9
liuxue <liuxue0905@hotmail.com>
2024-08-10 15:12:36 +0800

    修改UI

--- GoldenTimes/settings.py
+++ GoldenTimes/settings.py
@@ -10,11 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
 
-# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,7 +63,6 @@ ROOT_URLCONF = 'GoldenTimes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -88,7 +85,6 @@ WSGI_APPLICATION = 'GoldenTimes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -129,10 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/zh-hans/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "root", 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, "root", 'static')
+STATIC_ROOT = BASE_DIR / "root/static"
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "root", 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, "root", 'media')
+MEDIA_ROOT = BASE_DIR / "root/media"
 MEDIA_URL = '/media/'
 
 # STATICFILES_DIRS = [
