fa7a0681707b687ecbb1125d45fd3399630fdf55
liuxue <liuxue0905@hotmail.com>
2024-08-11 20:16:17 +0800

    修改BUG

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,22 @@
-import multiprocessing
+# import multiprocessing
 
 bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = multiprocessing.cpu_count() * 2
 
-print("bind", bind)
-print("workers", workers)
-print("threads", threads)
+workers = 2
+threads = 2
+
+timeout = 30
+backlog = 2048
+
+# worker_class = 'sync'
+# worker_connections = 1000
+# keepalive = 2
+#
+# spew = False
+
+# workers = multiprocessing.cpu_count() * 2 + 1
+# threads = multiprocessing.cpu_count() * 2
+#
+# print("bind", bind)
+# print("workers", workers)
+# print("threads", threads)
